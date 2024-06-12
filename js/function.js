152eae9eb23f6e7d042df9aea5373e9df4b90fe6
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-12 16:07:18 +0200

    sivluppata creata funzione per: - girare una stringa - comparazione per verifica se palindromi - generare numero randomi 1 - 5 - somma due numeri

--- js/function.js
+++ js/function.js
@@ -1,16 +1,31 @@
 // Funzione per la verifica se palindorma o meno
-let word;
 
-function palindro(word){
-    // Girare la parola al contratio
-    let revWord = reverseString(word)
-    console.log(word,revWord)
-    // Verificare se la prima stringa sia uguale alla parola
-    if (word === revWord){
-        return "Palindroma"
+// rigirare la parola
+function reverseWord(word) {
+    return word.split("").reverse().join("");
+}
+
+// Verificare che le parole siano uguali e quindi palindrome
+function palindorma(word) {
+    if (word === reverseWord(word)) {
+        return "palindroma"
     } else {
         return "Non palindroma"
     }
 }
+// Risultati
+// console.log(reverseWord(word))
+// console.log(palindorma(word));
 
-console.log(palindro("anna"))
+// Funzione per generare numero random da 1 a 5
+function random5(){
+    return num = Math.round(Math.random() * 4) + 1;
+}
+
+// Funzione somma 2 numeri
+function somma(a,b){
+    return a + b
+}
+
+
+
